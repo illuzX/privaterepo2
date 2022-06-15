86ed01a7498f0ec24e089659c43cf4f47ae9d93f
illuzX <98640218+illuzX@users.noreply.github.com>
2022-06-15 14:39:28 +0530

    update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/illuzX/privaterepo2 /privaterepo2 
+  git clone https://github.com/illuzX/privaterepo2 /app
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /app
