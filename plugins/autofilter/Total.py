5caa25b6c039ee2eb52707ed4b0fbc196d730891
illuzX <98640218+illuzX@users.noreply.github.com>
2022-06-16 23:50:34 +0530

    update Total.py

--- plugins/autofilter/Total.py
+++ plugins/autofilter/Total.py
@@ -3,6 +3,7 @@ from pyrogram import Client as illuzX, filters as Worker
 from plugins.database.autofilter_db import Media
 from plugins.database.users_chats_db import db
 import shutil
+import psutil
 from config import ADMINS
 logger = logging.getLogger(__name__)
 
